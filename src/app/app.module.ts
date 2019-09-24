7ce2a9daeb8072dbb8e36e7d6a5210d3657fa2b8
GUNJAN KUSHWAH <47561449+gunjan004@users.noreply.github.com>
2019-09-24 18:15:28 -0400

    Adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { ListComponent } from './list/list.component';
+import { AddComponent } from './add/add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ListComponent,
+    AddComponent
   ],
   imports: [
     BrowserModule,
